75175265c334b9487624b1b220bc89c68103f649
carlo <gaguigc@sg.ibm.com>
2022-05-05 21:38:00 +0800

    Added base code

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductFormComponent,
+    ShoppingCartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
